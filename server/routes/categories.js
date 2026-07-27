4c010a80e2a1556d2ebd2cddc32480029fb21c40
Devashish Rathod <rathod.devashish09@gmail.com>
2026-07-27 15:31:47 +0530

    Disable middleware for category route handlers
    Comment out middleware for category routes.

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -10,10 +10,10 @@ const {
   deleteCategory,
 } = require("../controllers/categories");
 
-router.post("/create", isAdmin, createCategory);
-router.get("/getAll", verifyJwtToken, getAllCategories);
-router.get("/get/:id", verifyJwtToken, getCategory);
-router.put("/update/:id", isAdmin, updateCategory);
-router.delete("/delete/:id", isAdmin, deleteCategory);
+router.post("/create",/* isAdmin,*/ createCategory);
+router.get("/getAll",/* verifyJwtToken, */ getAllCategories);
+router.get("/get/:id",/* verifyJwtToken,*/ getCategory);
+router.put("/update/:id",/* isAdmin,*/ updateCategory);
+router.delete("/delete/:id", /*isAdmin,*/ deleteCategory);
 
 module.exports = router;
