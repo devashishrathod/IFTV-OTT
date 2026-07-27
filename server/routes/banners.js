c826a911e63a65cbc3e3d55a41d50639f41349d8
Devashish Rathod <rathod.devashish09@gmail.com>
2026-07-27 15:29:31 +0530

    Disable middleware for banner routes
    Comment out middleware for routes in banners.js

--- server/routes/banners.js
+++ server/routes/banners.js
@@ -10,10 +10,10 @@ const {
   deleteBanner,
 } = require("../controllers/banners");
 
-router.post("/create", isAdmin, create);
-router.get("/getAll", verifyJwtToken, getAll);
-router.get("/get/:id", verifyJwtToken, get);
-// router.put("/update/:id", isAdmin, update);
-router.delete("/delete/:id", isAdmin, deleteBanner);
+router.post("/create", /*isAdmin,*/ create);
+router.get("/getAll", /*verifyJwtToken,*/ getAll);
+router.get("/get/:id", /*verifyJwtToken,*/ get);
+// router.put("/update/:id", /*isAdmin,*/ update);
+router.delete("/delete/:id", /*isAdmin,*/ deleteBanner);
 
 module.exports = router;
